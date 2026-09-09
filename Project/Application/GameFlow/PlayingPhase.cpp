a91629d5416f1fd112079d6a6f522590b3acfc89
saitoA1007 <k024g0004@g.neec.ac.jp>
2026-09-09 22:03:19 +0900

    ムービーの時に消えるようにする

--- Project/Application/GameFlow/PlayingPhase.cpp
+++ Project/Application/GameFlow/PlayingPhase.cpp
@@ -57,6 +57,10 @@ void PlayingPhase::OnEnter(GameFlowContext& context)
 	if (context.unitCountUI) {
 		context.unitCountUI->SetActive(true);
 	}
+
+	if (context.rocket) {
+		context.rocket->SetActive(true);
+	}
 }
 
 bool PlayingPhase::OnUpdate(GameFlowContext& context)
@@ -123,6 +127,7 @@ void PlayingPhase::OnExit(GameFlowContext& context)
 	if (context.rocket)
 	{
 		context.finalEnergy = context.rocket->GetEnergy(); 
+		context.rocket->SetActive(false);
 	}
 
 	if (context.unitCountUI) {
