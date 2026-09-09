93e95e42502c1cd219e4a845aa38c485fd3aff89
Shigemori Masato <k024g0027@g.neec.ac.jp>
2026-09-09 16:22:15 +0900

    敵の出現ファイルを追加

--- Project/Application/Enemy/EnemyEffectManager.h
+++ Project/Application/Enemy/EnemyEffectManager.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "IGameObject.h"
 #include"Application/Effect/EnemySpawnEffect.h"
+#include <ParticleBehavior.h>
 
 namespace GameEngine {
 	class ModelManager;
@@ -21,8 +22,13 @@ public:
 	// 敵の出現演出
 	void StartSpawnEffect(Vector3 pos);
 
+	void StartDeadEffect(Vector3 pos);
+
 private:
 
 	// 敵が出撃するビームの演出
 	std::vector<EnemySpawnEffect*> beamEffects_;
+
+	std::vector<GameEngine::ParticleBehavior*> deadEffects_;
+	int deadEffectIndex_ = 0;
 };
